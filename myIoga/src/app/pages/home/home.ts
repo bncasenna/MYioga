01240c4bfe20eb3a21e7856ad68b9e8189a8ba37
bncasenna <biancasenna@outlook.com.br>
2026-07-01 16:47:22 -0300

    atualizar

--- myIoga/src/app/pages/home/home.ts
+++ myIoga/src/app/pages/home/home.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Header } from '../../shared/components/header/header';
+import { Login } from '../../shared/components/login/login'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, Header],
+  imports: [CommonModule, Header, Login],
   templateUrl: './home.html',
-  styleUrls: ['./home.css']
+  styleUrl: './home.css' 
 })
-export class Home { }
+export class Home { 
+  mostrarLogin = false; 
+
+  abrirModalLogin() {
+    this.mostrarLogin = true;
+  }
+}
